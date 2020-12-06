bd4bc09a4b0631d39c40010120a3cdcd89c106bc
Yeison <yeison.eng@gmail.com>
2020-12-05 21:42:36 -0500

    remove PySide2 from install_requires and add Jinja2

--- setup.py
+++ setup.py
@@ -8,25 +8,24 @@ from setuptools import setup, find_packages
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='pyside-material',
+    name='qt-material',
     version='1.17',
-    packages=['pyside_material', 'pyside_material.resources'],
+    packages=['qt_material', 'qt_material.resources'],
 
     author='Yeison Cardona',
     author_email='yencardonaal@unal.edu.co',
     maintainer='Yeison Cardona',
     maintainer_email='yencardonaal@unal.edu.co',
 
-    download_url='https://github.com/UN-GCPDS/pyside-material',
+    download_url='https://github.com/UN-GCPDS/qt-material',
 
-    install_requires=['pyside2',
-                      ],
+    install_requires=['Jinja2'],
 
     python_requires='>=3.6',
 
     include_package_data=True,
     license='BSD-2-Clause',
-    description="PySide2 stylesheet.",
+    description="Qt Stylesheet for PySide2 and PyQt5.",
     #    long_description = README,
 
     classifiers=[
