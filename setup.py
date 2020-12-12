f06f8feb93aa4d7b224f10321fd294761df1aa76
Yeison <yeison.eng@gmail.com>
2020-12-12 14:20:05 -0500

    Release 2.1 with interactive example

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='qt-material',
-    version='2.0',
+    version='2.1',
     packages=['qt_material', 'qt_material.resources'],
 
     author='Yeison Cardona',
