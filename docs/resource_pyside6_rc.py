f815b4f4e72e134ba1e8c665ed23d820db80407b
Yeison <yeison.eng@gmail.com>
2020-12-13 11:21:33 -0500

    Removed references to primaryDarkColor

---
+++ docs/resource_pyside6_rc.py
@@ -0,0 +1,14 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.0.0
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
