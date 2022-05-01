046016ba6dcdea4c846ad9f15fa3fca9a2f8ee23
Yeison Cardona <yencardonaal@unal.edu.co>
2022-05-01 14:53:06 -0500

    Release 2.11

--- setup.py
+++ setup.py
@@ -2,35 +2,33 @@ import os
 from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-   README = readme.read()
+    README = readme.read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(
+    os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
+)
 
 setup(
     name='qt-material',
-    version='2.10',
-    packages=['qt_material',
-              'qt_material.resources',
-              'qt_material.resources.source'],
-
+    version='2.11',
+    packages=[
+        'qt_material',
+        'qt_material.resources',
+        'qt_material.resources.source',
+    ],
     author='Yeison Cardona',
     author_email='yencardonaal@unal.edu.co',
     maintainer='Yeison Cardona',
     maintainer_email='yencardonaal@unal.edu.co',
-
     download_url='https://github.com/UN-GCPDS/qt-material',
-
     install_requires=['Jinja2'],
-
     python_requires='>=3.7',
-
     include_package_data=True,
     license='BSD-2-Clause',
     description="Material inspired stylesheet for PySide2, PySide6, PyQt5 and PyQt6.",
     long_description=README,
     long_description_content_type='text/markdown',
-
     classifiers=[
         'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
@@ -39,11 +37,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
     ],
-
     entry_points={
-        "pyinstaller40": [
-            "hook-dirs = qt_material:get_hook_dirs"
-        ]
+        "pyinstaller40": ["hook-dirs = qt_material:get_hook_dirs"]
     },
-
 )
